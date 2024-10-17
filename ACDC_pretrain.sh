d03682c2b9a2d2545c52ed06c7d1c37407a13521
Sihan0229 <1109458800@qq.com>
2024-10-17 08:21:31 +0800

    sh files

---
+++ ACDC_pretrain.sh
@@ -0,0 +1,3 @@
+python /root/autodl-tmp/MONA/code/pretrain_2D.py \
+--root_path /root/autodl-tmp/MONA/data/ACDC \
+--exp ACDC/example_training
---
+++ ACDC_train.sh
@@ -0,0 +1,3 @@
+python /root/autodl-tmp/MONA/code/train_arco_2d.py \
+--root_path /root/autodl-tmp/MONA/data/ACDC \
+--resume ACDC/example_training
